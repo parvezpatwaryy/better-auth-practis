8b80097e72385f3553e68f14ef3f5cb16b14a4bc
parvezpatwaryy <tanjilapatwaryy@gmail.com>
2026-04-22 23:34:01 +0600

    signupPage complite

---
+++ src/app/auth/signin/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignUpPage from './../signup/page';
+
+const SigninPage = () => {
+  return (
+    <div>
+      <h2>please sign in</h2>
+    </div>
+  );
+};
+
+export default SigninPage;
